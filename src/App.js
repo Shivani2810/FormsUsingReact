74319a95430f82b023f7d8045b4c4790485a13ff
Shivani2810 <108538382+Shivani2810@users.noreply.github.com>
2026-02-17 13:41:54 +0530

    event.target= input tag being changed

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+  function changeHandler(event)
+      {
+        //  console.log(event.target.value);
+        console.log(event.target); 
+      }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+      <form>
+        <input
+        type="text"
+        placeholder="Enter your Name"
+        onChange={changeHandler}
+        />
+      </form>
     </div>
   );
 }
